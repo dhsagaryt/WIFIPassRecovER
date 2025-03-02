16391ea9f5fc92da791f80e791b1af42176994c2
Sagar Mondal <128616069+dhsagaryt@users.noreply.github.com>
2025-03-02 16:53:40 +0530

    Update WIFIPassRecovER 2.3.bat

--- WIFIPassRecovER 2.3.bat
+++ WIFIPassRecovER 2.3.bat
@@ -28,3 +28,29 @@ cls & color 0A
 if _%1_==_Main_  goto :Main
 Set Count=0
 Set L=0
+
+Mode con cols=75 lines=8
+cls & color 0A
+	echo __          _______ ______ _____  _____              _____                     ______ _____  
+	echo \ \        / /_   _^|  ____^|_   _^|^|  __ \            ^|  __ \                   ^|  ____^|  __ \ 
+	echo  \ \  /\  / /  ^| ^| ^| ^|__    ^| ^|  ^| ^|__) ^|_ _ ___ ___^| ^|__) ^|___  ___ _____   _^| ^|__  ^| ^|__) ^|
+	echo   \ \/  \/ /   ^| ^| ^|  __^|   ^| ^|  ^|  ___/ _` / __/ __^|  _  // _ \/ __/ _ \ \ / /  __^| ^|  _  / 
+	echo    \  /\  /   _^| ^|_^| ^|     _^| ^|_ ^| ^|  ^| (_^| \__ \__ \ ^| \ \  __/ (_^| (_) \ V /^| ^|____^| ^| \ \ 
+	echo     \/  \/   ^|_____^|_^|    ^|_____^|^|_^|   \__,_^|___/___/_^|  \_\___^|\___\___/ \_/ ^|______^|_^|  \_\
+	echo.
+	echo(
+if _%1_==_Main_  goto :Main
+Set Count=0
+Set L=0
+
+REM ===================== Get Admin
+:getAdmin
+    echo               %~nx0 : self elevating
+    set vbs=%temp%\getAdmin.vbs
+(
+	echo Set UAC = CreateObject^("Shell.Application"^)
+	echo UAC.ShellExecute "%~s0", "Main %~sdp0 %*", "", "runas", 1
+)> "%vbs%"
+    "%temp%\getAdmin.vbs"
+    del "%temp%\getAdmin.vbs"
+goto :eof
