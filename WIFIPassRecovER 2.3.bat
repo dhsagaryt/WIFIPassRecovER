b15c89084549f2442b5f878b0905c5f71fa641d5
Sagar Mondal <128616069+dhsagaryt@users.noreply.github.com>
2025-03-02 16:51:30 +0530

    Update WIFIPassRecovER 2.3.bat

--- WIFIPassRecovER 2.3.bat
+++ WIFIPassRecovER 2.3.bat
@@ -6,3 +6,12 @@ Set "proName=WIFIPassRecovER"
 Set "proVer=2.3"
 
 Title  %proName% by %autName%
+
+:: Program File Check!!
+IF NOT "%~n0"=="%proName% %proVer%" (
+	echo.
+    ECHO Incorrect Namespace! This program made by %autName%".
+    timeout /t 5 /nobreak
+    exit
+)
+
